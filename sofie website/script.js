185d4087bd5fc8d01b8d704647417df679a34b27
Akyra Daffa <akyra.daffa@gmail.com>
2024-04-20 09:33:04 +0800

    Delete sofie website directory

--- sofie website/script.js
+++
@@ -1,9 +0,0 @@
-function checkPassword() {
-    var password = document.getElementById('passwordInput').value;
-    // Replace 'your_password' with the actual password
-    if (password === 'yaye') {
-        window.location.href = 'https://sofriggies.framer.ai'; // Redirect to the desired page
-    } else {
-        alert('try again baby, u can do it!');
-    }
-}
